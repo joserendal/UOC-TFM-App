cb1dea048fef52d9c39d9efd075dd27f6530f7c1
Jose Enrique <jalvarezrend@uoc.edu>
2019-04-18 11:20:06 +0200

    Datos personales segment

--- gGym/gGym/UserAccountCreationViewController.swift
+++ gGym/gGym/UserAccountCreationViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class UserAccountCreationViewController : UIViewController {
     
-    // UIViewController
+    // UIViewController - Segment and Views
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var personalDataViewContainer: UIView!
+    @IBOutlet weak var centerDataViewContainer: UIView!
+    
     // User data
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var surnameField: UITextField!
@@ -25,18 +28,33 @@ class UserAccountCreationViewController : UIViewController {
     @IBOutlet weak var tipeField: UITextField!
     @IBOutlet weak var addressField: UITextField!
     
-    // Configurar barra de navegación
+    // Configure navigation bar
     override func viewWillAppear(_ animated: Bool) {
         // View title
         navigationItem.title = "Crear una cuenta"
         // Create button
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Crear", style: .plain, target: self, action: #selector(UserAccountCreationViewController.createButtonTapped))
-        
+        // Segment control initial state
+        personalDataViewContainer.isHidden = false
+        centerDataViewContainer.isHidden = true
     }
     
     func createButtonTapped() {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    // User clicks in Segment change
     @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        // Get the index clicked
+        let index = sender.selectedSegmentIndex
+        // swith views according to the index
+        switch index {
+        case 1:
+            personalDataViewContainer.isHidden = true
+            centerDataViewContainer.isHidden = false
+        default:
+            personalDataViewContainer.isHidden = false
+            centerDataViewContainer.isHidden = true
+        }
     }
 }
